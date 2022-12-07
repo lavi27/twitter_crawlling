d94507727ce4cbab3ae00c1ed930b3c81160e674
lavi <62757958+lavi27@users.noreply.github.com>
2022-12-08 02:39:32 +0900

    fix: get entire page, fix: error handling, add: ui

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ import { TwitterApi } from 'twitter-api-v2'
 import moment from 'moment'
 import { Bearer_token } from '../config.json'
 import Axios from 'axios'
+import cliProgress from 'cli-progress'
+// import { getLikedTweetsDatas } from '../sdfg/getLikedTweets'
 
 interface Tweets {
   url: any;
@@ -12,29 +14,50 @@ interface Tweets {
   medias: any
 }
 
+const bar1 = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
+
 const twitterClient = new TwitterApi(Bearer_token)
 const client = twitterClient.readOnly
 
 let userid: string
-userid = '1491856273582673926'
+// userid = '1491856273582673926'
+userid = '1533011206448918530'
 
 const getLikedTweetsDatas = async (userid: string) => {
+  console.log('Get liked tweets.')
   let tweets: Tweets[] = []
   let next_token: string|null = null
 
-  for(let i=0; i < 20; i++) {
+
+  while (true) {
     const result: any = await getLikedTweetsData(userid, next_token)
+
+    if(result.end) {
+      return tweets
+    }
+
     tweets = tweets.concat(result.tweets)
     next_token = result.next_token
   }
 
-  return tweets
+  // for(let i=0; i < 5; i++) {
+  //   const result: any = await getLikedTweetsData(userid, next_token)
+
+  //   if(result.end) {
+  //     console.log('done.')
+  //     return tweets
+  //   }
+
+  //   tweets = tweets.concat(result.tweets)
+  //   next_token = result.next_token
+  // }
+
+  // console.log('done.')
+  // return tweets
 }
 
 const getLikedTweetsData = async (userid: string, next_token: string|null) => {
   try {
-    const url = `users/${userid}/liked_tweets`
-
     const options: any = {
       max_results: 100,
       "expansions": "attachments.media_keys,author_id",
@@ -44,10 +67,15 @@ const getLikedTweetsData = async (userid: string, next_token: string|null) => {
       options.pagination_token = next_token
     }
 
-    const response = await client.v2.get(url, options);
+    const response = await client.v2.get(`users/${userid}/liked_tweets`, options);
 
     const data = response.data
     const meta = response.meta
+
+    if (meta.result_count == 0) {
+      return { end: true }
+    }
+
     const includes_media = response.includes.media
     const includes_users = response.includes.users
 
@@ -68,18 +96,14 @@ const getLikedTweetsData = async (userid: string, next_token: string|null) => {
       tweets.push({url, name: user.name, username: user.username, id: data.id, medias})
     })
 
-    return { tweets, next_token: meta.next_token };
-
-    //[이미지 하나]
-    // 포함될 정보: 글 url, 유저 닉네임(@유저 고닉) x, 트위터 로고
-    // 이름: 글 url x
-  } catch(err) {
-    console.error(err)
-    process.exit(1)
+    return { tweets, next_token: meta.next_token, end: false };
+  } catch(err: any) {
+    throw new Error(err)
   }
 }
 
 const makeJson = (fileDir: string, filename: string, mediadata: object) => {
+  console.log('Making json file.')
   try {
     const json = JSON.stringify(mediadata, null, 2)
     
@@ -88,9 +112,9 @@ const makeJson = (fileDir: string, filename: string, mediadata: object) => {
     }
 
     fs.writeFileSync(`${fileDir}/${filename}.json`, json)
-  } catch(err) {
-    console.error(err)
-    process.exit(1)
+    console.log('done.')
+  } catch(err: any) {
+    throw new Error(err)
   }
 }
 
@@ -99,7 +123,7 @@ const downloadImage = async (url: string, fileDir: string, filename: string) => 
     const filenameExtension = url.split('.').slice(-1)[0];
     const path = `${fileDir}/${filename}.${filenameExtension}`;
 
-    const response = await Axios.get(url, { responseType: 'stream' });
+    const response = await Axios.get(url, { responseType: 'stream' })
 
     if(!fs.existsSync(fileDir)) {
       fs.mkdirSync(fileDir)
@@ -107,20 +131,54 @@ const downloadImage = async (url: string, fileDir: string, filename: string) => 
 
     await response.data.pipe(fs.createWriteStream(path))
     return path
-  } catch(err) {
-    console.error(err)
-    process.exit(1)
+  } catch(err: any) {
+    throw new Error(err)
   }
 }
 
+const convertMsToTime = (ms: number) => {
+  let seconds = Math.floor(ms / 1000);
+  let minutes = Math.floor(seconds / 60);
+  let hours = Math.floor(minutes / 60);
+
+  seconds = seconds % 60;
+  minutes = minutes % 60;
+  hours = hours % 24;
+
+  let result: string = '';
+
+  if(hours) {
+    result += `${hours}h `;
+  }
+  if(minutes) {
+    result += `${minutes}m `;
+  }
+  if(seconds) {
+    result += `${seconds}s`;
+  }
+  
+  return result;
+}
+
+const wait = (timeout: number) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(null);
+    }, timeout);
+  });
+}
+
 const app = async () => {
+  const time_start = Date.now()
   const likedTweetsData = await getLikedTweetsDatas(userid)
-  const timestamp = Date.now()
-  const fileDir = `./out/${userid}_${timestamp}`
+  const fileDir = `./out/${userid}_${time_start}`
 
   makeJson(fileDir, 'info', likedTweetsData)
 
+  console.log('Downloading image files.')
+  bar1.start(likedTweetsData.length, 0);
   likedTweetsData.forEach((tweet: Tweets, dataIndex:number) => {
+    bar1.increment();
     if(tweet.medias.length < 1) {
       return
     }
@@ -133,9 +191,11 @@ const app = async () => {
       downloadImage(mediaUrl, fileDir, `${dataIndex+1}_${mediaIndex+1}`)
     })
   });
+  bar1.stop();
+  console.log('Done.')
 
-  console.log("Success!")
-  process.exit(0)
+  const elapsedMSec = new Date().getTime() - time_start;
+  console.log(`\nSuccess! (${convertMsToTime(elapsedMSec)})`)
 }
 
-app();
+app()
